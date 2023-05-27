be23bc5f207cac11c23eb2c353a129d81ef5f7be
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-27 13:43:01 +0300

    eds

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+import jsconfigPaths from "vite-jsconfig-paths";
 
 export default defineConfig({
   base: "/vite-js-template/",
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "./src/partials"),
+    }),
+    jsconfigPaths(),
+  ],
 });
